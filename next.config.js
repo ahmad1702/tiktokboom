037acdaa5f6c641c22f016166b84ad988ed9b07d
ahmad1702 <ahmad1702@gmail.com>
2023-06-30 15:13:46 -0500

    inti build fix via next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
